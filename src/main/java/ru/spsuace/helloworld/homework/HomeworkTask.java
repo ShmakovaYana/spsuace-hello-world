562d8899aafb49aae377c0a4e26cb6af44c94515
Yana <yanaaaaaa1998@mail.ru>
2019-10-02 14:04:37 +0300

    done

--- src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
+++ src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
@@ -10,7 +10,12 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+
+        double integral = 0;
+        for (double i = a; i < b; i = i + delta) {
+            integral += function.applyAsDouble(i) * delta;
+        }
+        return integral;
     }
 
     /**
@@ -18,16 +23,33 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        long Change_a = a;
+        long number;
+        long MaxNumber = 0;
+        long amount = 1;
+        long index = 0;
+        do {
+            number = Change_a % 10;
+            Change_a /= 10;
+            amount++;
+            if (number >= MaxNumber) {
+                MaxNumber = number;
+                index = amount;
+            }
+
+        } while (Change_a != 0);
+        return (byte) (amount - index + 1);
     }
-
-
     /**
      * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        double a = (y2 - y1) * x3;
+        double b = x2 * y1 - x1 * y2;
+        double c = x2 - x1;
+
+        return (a + b) / c;
     }
 
     /**
@@ -36,7 +58,7 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        return  (Math.abs ((double)(x1 * y2 + x2 * y3 + x3 * y4 + x4 * y1 - x2 * y1 - x3 * y2 - x4 * y3 - x1 * y4))/2);
     }
 
 }
